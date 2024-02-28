cab749362b2ee5fadd912391a58e6ab7a76f1d4a
Jackson Lawrence <fi19710@bristol.ac.uk>
2024-02-28 19:28:58 +0000

    CSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
 	import '../app.pcss';
+	import { DarkMode } from 'flowbite-svelte';
 </script>
+
+<div
+	class="absolute flex w-screen flex-row items-center justify-between border-b-2 px-8 py-2 dark:border-gray-600 dark:bg-gray-800"
+>
+	<nav>
+		<a href="/">Home</a>
+	</nav>
+	<DarkMode />
+</div>
 
 <slot />
